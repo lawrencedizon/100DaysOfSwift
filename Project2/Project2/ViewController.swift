6abed3582443f0b73c6150c28377d63541b1f994
Lawrence Dizon <lawdizon8@gmail.com>
2020-10-16 01:13:08 -0700

    added button titles and game score

--- Project2/Project2/ViewController.swift
+++ Project2/Project2/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     
     var countries = [String]()
     var score = 0
+    var correctAnswer = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,14 +31,40 @@ class ViewController: UIViewController {
         button3.layer.borderColor = UIColor.lightGray.cgColor
         
         askQuestion() 
-    }
+    } 
     
-    func askQuestion(){
+    func askQuestion(action: UIAlertAction! = nil){
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2) //an integer value between 0 and 2 inclusive.
+        
+        
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        title = countries[correctAnswer].uppercased()
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+            
+        }else {
+            title = "Wrong"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your Score is \(score)", preferredStyle: .alert)
+        
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        
+        present(ac, animated: true)
+    }
+    
+    
+    
 }
 
