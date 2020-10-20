73e540c80bde9ff6c5247e32816af8ea36d0a6e8
Lawrence Dizon <lawdizon8@gmail.com>
2020-10-19 19:59:46 -0700

    changed UIView to WKWebView

--- Project4/Project4/ViewController.swift
+++ Project4/Project4/ViewController.swift
@@ -6,12 +6,25 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView(){ //loadView gets called before viewDidLoad
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let url = URL(string: "https://hackingwithswift.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
     }
 
 
