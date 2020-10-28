98de0affc5e40d69915096aa21bb3635c0a0eb57
Lawrence Dizon <lawdizon8@gmail.com>
2020-10-28 15:21:11 -0700

    Initial commit

---
+++ Project5/Project 5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 5
+//
+//  Created by Lawrence Dizon on 10/28/20.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
