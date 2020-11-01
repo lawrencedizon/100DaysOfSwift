7b00829752c37da22e406b74a2c1d324bd31f1f4
Lawrence Dizon <lawdizon8@gmail.com>
2020-11-01 15:57:31 -0800

    MVC structure organization

---
+++ Project4/Project4/Model/WebsiteData.swift
@@ -0,0 +1,12 @@
+//
+//  WebsiteData.swift
+//  Project4
+//
+//  Created by Lawrence Dizon on 11/1/20.
+//
+
+import Foundation
+
+struct WebsiteData{
+    let websites = ["facebook.com","google.com","hackingwithswift.com"]
+}
