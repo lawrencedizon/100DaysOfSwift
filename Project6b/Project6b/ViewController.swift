9d4166933b3a29fb0f7ee829b64d18cacdf82476
Lawrence Dizon <lawdizon8@gmail.com>
2020-11-02 18:25:08 -0800

    Initial commit

---
+++ Project6b/Project6b/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project6b
+//
+//  Created by Lawrence Dizon on 11/2/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
