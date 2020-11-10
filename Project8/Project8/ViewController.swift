5dd969fa50acbb338f03b52899366e87c8579cc1
Lawrence Dizon <lawdizon8@gmail.com>
2020-11-09 22:13:45 -0800

    Add programatic buttons

--- Project8/Project8/ViewController.swift
+++ Project8/Project8/ViewController.swift
@@ -7,11 +7,39 @@
 
 import UIKit
 
+//
+//MARK:- ViewController
+//
 class ViewController: UIViewController {
-
+    var cluesLabel: UILabel!
+    var answerLabel: UILabel!
+    var currentAnswer: UITextField!
+    var scoreLabel: UILabel!
+    var letterButtons = [UIButton]()
+    
+    override func loadView(){
+        view = UIView()
+        view.backgroundColor = .white
+        
+        //more code to come
+        scoreLabel = UILabel()
+        scoreLabel.translatesAutoresizingMaskIntoConstraints = false
+        scoreLabel.textAlignment = .right
+        scoreLabel.text = "Score: 0"
+        view.addSubview(scoreLabel)
+        
+        NSLayoutConstraint.activate([
+        scoreLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+    
+        scoreLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor)
+        
+        ])
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("Tets")
+        
     }
 
 
