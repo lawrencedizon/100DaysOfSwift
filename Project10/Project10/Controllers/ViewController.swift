f2795dee68646fcf3e68015e997b240a380f472d
Lawrence Dizon <lawdizon8@gmail.com>
2020-11-17 13:21:55 -0800

    Update UICollectionView properties

--- Project10/Project10/Controllers/ViewController.swift
+++ Project10/Project10/Controllers/ViewController.swift
@@ -6,12 +6,40 @@
 //
 
 import UIKit
-
+//
+//ViewController
+//
 class ViewController: UICollectionViewController {
+    
+    //
+    //MARK:- ViewController Properties
+    //
+    
+    
+    //
+    //MARK:- ViewController View States
+    //
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    
+    //
+    //MARK:- CollectionView Properties
+    //
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            
+            fatalError("Unable to dequeue PersonCell.")
+        }
+
+        return cell
     }
 
 
