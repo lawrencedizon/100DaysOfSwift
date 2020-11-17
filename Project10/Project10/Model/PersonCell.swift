094dc9d8d3c64363ee09af199eaee7b8f307001e
Lawrence Dizon <lawdizon8@gmail.com>
2020-11-17 11:53:29 -0800

    Create Person UICollectionViewCell

---
+++ Project10/Project10/Model/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Lawrence Dizon on 11/17/20.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
