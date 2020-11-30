3a80b27beaeccb367f5b4791dc6a6ebf6e13b6c3
ldizon8 <lawdizon8@gmail.com>
2020-11-30 12:31:41 -0800

    Add PersonCell and Person class
    PersonCell displays the Person information onto the UI

---
+++ Project10/Project10/Model/Person.swift
@@ -0,0 +1,12 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Lawrence Dizon on 11/30/20.
+//
+
+import UIKit
+
+class Person: NSObject {
+
+}
---
+++ Project10/Project10/View/PersonCell.swift
@@ -0,0 +1,12 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Lawrence Dizon on 11/30/20.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+}
