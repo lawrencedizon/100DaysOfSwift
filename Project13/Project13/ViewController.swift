e7ba63e343c996a039108a30e229e5256cbb01ce
ldizon8 <lawdizon8@gmail.com>
2020-12-02 16:51:13 -0800

    Add photo saving functionality

--- Project13/Project13/ViewController.swift
+++ Project13/Project13/ViewController.swift
@@ -78,6 +78,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
      
     @IBAction func save(_ sender: Any) {
+        guard let image = imageView.image else { return }
+        
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(image(_:  didFinishSavingWithError:contextInfo: )), nil)
+    
     }
     @IBAction func intensityChanged(_ sender: Any) {
         applyProcessing()
@@ -108,6 +112,20 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             let processedImage = UIImage(cgImage: cgImage)
             imageView.image = processedImage
         }
+    }
+    
+    @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer){
+        if let error = error{
+            let ac = UIAlertController(title: "Save error", message: error.localizedDescription , preferredStyle: .alert )
+            ac.addAction(UIAlertAction(title: "Ok", style: .default))
+            present(ac, animated: true)
+            
+        }else {
+            let ac = UIAlertController(title: "Saved!", message: "Your altered image has been saved to your photos", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Ok", style: .default))
+            present(ac, animated: true)
+        }
+        
     }
     
      
