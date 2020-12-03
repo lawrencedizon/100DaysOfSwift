59a5bf6f29faa74bd69643d7281d5f1428ee1df5
ldizon8 <lawdizon8@gmail.com>
2020-12-03 10:28:29 -0800

    Created penguin whack slots

--- Project14/Project14/WhackSlot.swift
+++ Project14/Project14/WhackSlot.swift
@@ -5,8 +5,28 @@
 //  Created by Lawrence Dizon on 12/3/20.
 //
 
+import SpriteKit
 import UIKit
 
 class WhackSlot: SKNode {
+    var charNode: SKSpriteNode!
+    
+    func configure(at position: CGPoint){
+        self.position = position
+        
+        let sprite = SKSpriteNode(imageNamed: "whackHole")
+        addChild(sprite)
+        
+        let cropNode = SKCropNode()
+        cropNode.position = CGPoint(x: 0, y: 15)
+        cropNode.zPosition = 1
+        cropNode.maskNode = SKSpriteNode(imageNamed: "whackMasked")
+        
+        charNode = SKSpriteNode(imageNamed: "penguinGood")
+        charNode.position = CGPoint(x: 0, y: -110)
+        charNode.name = "character"
+        cropNode.addChild(charNode)
+        addChild(cropNode)
+    }
 
 }
