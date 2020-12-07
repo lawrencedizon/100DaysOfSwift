7a184ac92f9e4569c8f4221eb19c7d2b29b1a6df
ldizon8 <lawdizon8@gmail.com>
2020-12-07 14:26:25 -0800

    Add Capital Model class and MKAnnotations

---
+++ Project16/Project16/Controller/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Project16
+//
+//  Created by Lawrence Dizon on 12/7/20.
+//
+
+import MapKit
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet var mapView: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let london = Capital(title: "London", coordinate: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275),info: "Home to the 2012 Summer Olympics.")
+        
+        let oslo = Capital(title: "Oslo", coordinate: CLLocationCoordinate2D(latitude: 59.95, longitude: 10.75), info: "Founded over a thousand years ago.")
+        
+        let paris = Capital(title: "Paris", coordinate: CLLocationCoordinate2D(latitude: 48.8567, longitude: 2.3508), info: "Often called the City of Light.")
+        
+        let rome = Capital(title: "Rome", coordinate: CLLocationCoordinate2D(latitude: 41.9, longitude: 12.5), info: "Has a whole country inside it.")
+        
+        let washington = Capital(title: "Washington DC", coordinate: CLLocationCoordinate2D(latitude: 39.89511, longitude: -77.036667), info: "Named after George himself.")
+
+        mapView.addAnnotations([london, oslo, paris, rome, washington]) 
+        
+        //Two ways to add annotations to mapView but former is preferred
+        //        mapView.addAnnotation(london)
+        //        mapView.addAnnotation(oslo)
+        //        mapView.addAnnotation(paris)
+        //        mapView.addAnnotation(rome)
+        //        mapView.addAnnotation(washington)
+        
+    }
+
+
+}
+
