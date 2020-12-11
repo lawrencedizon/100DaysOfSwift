071741122e9549fafdbcc61c463440a34487ab58
ldizon8 <lawdizon8@gmail.com>
2020-12-10 19:59:11 -0800

    Implement firework explosion interaction

--- Project20/Project20/GameScene.swift
+++ Project20/Project20/GameScene.swift
@@ -123,7 +123,6 @@ class GameScene: SKScene {
                 }
             }
             
-           
             node.name = "selected"
             node.colorBlendFactor = 0
             
@@ -144,8 +143,46 @@ class GameScene: SKScene {
         for (index, firework) in fireworks.enumerated().reversed() {
             if firework.position.y > 900 {
                 fireworks.remove(at: index)
-                firework.removeFromParent() 
+                firework.removeFromParent()
             }
+        }
+    }
+    
+    func explode(firework: SKNode){
+        if let emitter = SKEmitterNode(fileNamed: "explode"){
+            emitter.position = firework.position
+            addChild(emitter)
+        }
+        
+        firework.removeFromParent()
+    }
+    
+    func explodeFireworks(){
+        var numExploded = 0
+        for (index, fireworkContainer) in fireworks.enumerated().reversed() {
+            guard let firework = fireworkContainer.children.first as?
+                    SKSpriteNode else { continue }
+            
+            if firework.name == "selected" {
+                 explode(firework: fireworkContainer)
+                fireworks.remove(at: index)
+                numExploded += 1
+            }
+        }
+        
+        switch numExploded {
+        case 0:
+            break
+        case 1:
+            score += 200
+        case 2:
+            score += 500
+        case 3:
+            score += 1500
+        case 4:
+            score += 2500
+        default:
+            score += 4000
         }
     }
 }
