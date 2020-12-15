7c825623aa3430a009fbb970da505f7c96f5feea
ldizon8 <lawdizon8@gmail.com>
2020-12-14 18:15:57 -0800

    Project 22 Complete with beacon detection

--- Project22/Project22/ViewController.swift
+++ Project22/Project22/ViewController.swift
@@ -30,8 +30,54 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
                 if CLLocationManager.isRangingAvailable() {
                     // do stuff
+                    startScanning()
                 }
             }
+        }
+    }
+    
+    func startScanning(){
+        let uuid = UUID(uuidString: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5")!
+        
+        let beaconRegion = CLBeaconRegion(uuid: uuid, major: 123, minor: 456, identifier: "MyBeacon")
+        
+        locationManager?.startMonitoring(for: beaconRegion)
+        locationManager?.startRangingBeacons(in: beaconRegion)
+        
+    }
+    
+    func update(distance: CLProximity){
+        UIView.animate(withDuration: 1){
+            switch distance {
+            case .unknown:
+                self.view.backgroundColor = .gray
+                self.distanceReading.text = "UNKNOWN"
+                
+            case .far:
+                self.view.backgroundColor = .blue
+                self.distanceReading.text = "FAR"
+                
+            case .near:
+                self.view.backgroundColor = .orange
+                self.distanceReading.text = "NEAR"
+                
+            case .immediate:
+                self.view.backgroundColor = .red
+                self.distanceReading.text = "RIGHT HERE"
+                
+            default:
+                self.view.backgroundColor = .gray
+                self.distanceReading.text = "UNKNOWN"
+                
+            }
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
+        if let beacon = beacons.first {
+            update(distance: beacon.proximity)
+        }else {
+            update(distance: .unknown)
         }
     }
     
