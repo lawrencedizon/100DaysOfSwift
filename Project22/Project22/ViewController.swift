4eb758e367394343c8bf238f664706cd7676b28b
ldizon8 <lawdizon8@gmail.com>
2020-12-14 17:21:42 -0800

    Initial commit

---
+++ Project22/Project22/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project22
+//
+//  Created by Lawrence Dizon on 12/14/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
