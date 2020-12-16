d19ddb959d736713d6ffa6412ed9a27c775ff217
ldizon8 <lawdizon8@gmail.com>
2020-12-16 13:37:31 -0800

    Add more Swift String examples

---
+++ Project24/Project24/Project24.playground/Pages/Untitled Page 2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,65 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var str = "Hello, playground"
+
+let password = "12345"
+
+password.hasPrefix("123")
+password.hasSuffix("456")
+
+extension String {
+    func deletingPrefix(_ prefix: String) -> String {
+        guard self.hasPrefix(prefix) else { return self}
+        
+        return String(self.dropFirst(prefix.count))
+        
+        
+    }
+    
+    func deletingSuffix(_ suffix: String) -> String {
+        guard self.hasSuffix(suffix) else { return self}
+        
+        return String(self.dropLast(suffix.count))
+        
+    }
+}
+    
+    let weather = "it's going to rain"
+    
+    print(weather.capitalized)
+
+extension String {
+    var capitalizedFirst: String {
+        guard let firstLetter = self.first else { return "" }
+        
+        return firstLetter.uppercased() + self.dropFirst()
+    }
+}
+
+let input = "Swift is like Objective-C without the C"
+input.contains("Swift")
+
+let languages = ["Python", "Ruby","Swift"]
+languages.contains("Swift")
+
+extension String {
+    
+    func containsAny(of array: [String]) -> Bool {
+        for item in array {
+            if self.contains(item){
+                return true
+            }
+        }
+        
+        return false
+    }
+    
+    
+}
+
+input.containsAny(of: languages)
+
+languages.contains(where: input.contains)
+
