1503fc3e210867436dff88eb7103dd55e9b860f9
ldizon8 <lawdizon8@gmail.com>
2020-12-16 13:25:08 -0800

    Write String extension - String is not array example

--- Project24/Project24/Project24.playground/Contents.swift
+++ Project24/Project24/Project24.playground/Contents.swift
@@ -1,3 +1,20 @@
 import UIKit
 
 var str = "Hello, playground"
+
+let name = "Taylor"
+
+for letter in name{
+    print("Give me a \(letter)")
+    
+}
+
+let letter = name[name.index(name.startIndex, offsetBy: 3)]
+
+extension String {
+    subscript(i: Int) -> String {
+        return String(self[index(startIndex, offsetBy: i)])
+    }
+}
+
+let letter2 = name[3]
