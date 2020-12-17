ffab8b2b5e43252cf35a5893c253ab15d216dcb4
ldizon8 <lawdizon8@gmail.com>
2020-12-17 10:38:29 -0800

    Add game level background

--- Project26/Project26/GameScene.swift
+++ Project26/Project26/GameScene.swift
@@ -18,6 +18,11 @@ class GameScene: SKScene {
     }
     
     override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "background.jpg")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
         loadLevel()
     }
     
