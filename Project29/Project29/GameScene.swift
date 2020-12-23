1872b0b3b772b3c596ab88460ac73aa6f00a5049
ldizon8 <lawdizon8@gmail.com>
2020-12-22 21:51:56 -0800

    Initial commit

---
+++ Project29/Project29/GameScene.swift
@@ -0,0 +1,15 @@
+//
+//  GameScene.swift
+//  Project29
+//
+//  Created by Lawrence Dizon on 12/22/20.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+}
